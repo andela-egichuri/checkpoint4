4bbd3dce469077507924ffc4c723534b78793ef8
Eric G <eric.gichuri@andela.com>
2016-02-10 18:46:49 +0300

    [Feature #113377367] Initial setup for the landing page
    Add a basic landing page.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+
+def index(request):
+    """Application Dashboard"""
+    if request.user.is_authenticated() and request.user.is_active:
+        return HttpResponseRedirect('/dashboard')
+
+    content = {}
+    return render(request, 'index.html', content)
+
+
+@login_required
+def dashboard(request):
+    """Application dashboard"""
+    return render(request, 'dashboard.html', {})
--- picmate/urls.py
+++ picmate/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from app import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
+    url(r'^dashboard/', views.dashboard, name='dashboard'),
 ]
